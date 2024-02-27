709377ac5acfe5a6e725eb85710a77d6328471b5
Luiz Augusto Volpi Nascimento <luizvnasc@users.noreply.github.com>
2024-02-27 10:01:36 +0000

    Criado tabelas e funções do banco de dados

--- sql/init.sql
+++ sql/init.sql
@@ -1,8 +1,31 @@
+DROP TABLE IF EXISTS transacaos;
+DROP TABLE IF EXISTS clientes;
+
+
 CREATE TABLE transacaos (
     id SERIAL PRIMARY KEY,
     cliente_id BIGINT NOT NULL,
     valor BIGINT NOT NULL,
     tipo CHAR(1) NOT NULL,
     descricao VARCHAR(128) NOT NULL,
-    created_at TIMESTAMP NOT NULL
-)
+    criado_em TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    limite BIGINT NOT NULL,
+    saldo BIGINT NOT NULL DEFAULT 0
+);
+
+DO $$
+BEGIN
+  INSERT INTO clientes (limite)
+  VALUES
+    (100000),
+    (80000),
+    (1000000),
+    (10000000),
+    (500000);
+END; $$
+
+
